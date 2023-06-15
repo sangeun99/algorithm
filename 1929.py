59f3a9c99c6d0e8d5888c31664392da369d2d5c9
sangeun <sangeun99@khu.ac.kr>
2023-06-16 02:06:49 +0900

    1929: 시간 초과 문제 해결 (list이용)

--- 1929.py
+++ 1929.py
@@ -1,19 +1,20 @@
-def prime(n) :
-  if (n == 1) :
-    return False
-  for i in range(2, n):
-    if n % i == 0 :
-      return False
-  return True
-
-def all_prime(m ,n) :
-  for i in range(m, n + 1) :
-    if (prime(i)):
-      print(i)
+def makePrimeNumList(n) :
+  primeNumList = list(range(0, n+1))
+  primeNumList[1] = 0
+  for i in range(2, n) :
+    if primeNumList[i] == 0 :
+      continue
+    for j in range(2 * i, n + 1, i) :
+      primeNumList[j] = 0
+  return primeNumList
 
 def main():
   m, n = map(int, input().split())
-  print(all_prime(m, n))
+  
+  primeNumList = makePrimeNumList(n)
+  for i in range (m, n+1) :
+    if (primeNumList[i] > 0) :
+      print(primeNumList[i])
 
 if __name__== "__main__" :
-    main()
+  main()
