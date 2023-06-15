7221be799f293fc6bd5baa309c1bad9361c4dc3c
sangeun <sangeun99@khu.ac.kr>
2023-06-16 01:29:41 +0900

    1929 : 소수 구하기

---
+++ 1929.py
@@ -0,0 +1,20 @@
+def prime(n) :
+  if (n == 1) :
+    return False
+  for i in range(2, n):
+    if n % i == 0 :
+      return False
+  return True
+
+def all_prime(m ,n) :
+  for i in range(m, n + 1) :
+    if (prime(i)):
+      print(i)
+
+def main():
+  m = int(input())
+  n = int(input())
+  print(all_prime(m, n))
+
+if __name__== "__main__" :
+    main()
