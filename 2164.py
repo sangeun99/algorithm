c8d42a37d30d92e7011fb9c8824360a7cc6a4f99
sangeun <sangeun99@khu.ac.kr>
2023-06-16 15:15:48 +0900

    2164: collections.deque 이용

--- 2164.py
+++ 2164.py
@@ -1,10 +1,12 @@
+from collections import deque
+
 def main() :
   N = int(input())
-  n_list = list(range(1, N + 1))
+  n_list = deque(range(1, N + 1))
 
   while (len(n_list) > 1) :
-    n_list.pop(0)
-    n_list.append(n_list.pop(0))
+    n_list.popleft()
+    n_list.append(n_list.popleft())
   
   print(n_list[0])
 
