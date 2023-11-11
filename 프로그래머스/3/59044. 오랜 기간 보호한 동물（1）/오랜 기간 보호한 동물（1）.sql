9aa2963e6b5cfedb92d818ad8453ec9107862ba1
tori <63828057+sangeun99@users.noreply.github.com>
2023-11-11 14:43:52 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,8 +1,9 @@
-SELECT  *
-FROM    (
-    SELECT  NAME, DATETIME
-    FROM    ANIMAL_INS
-    WHERE   ANIMAL_ID NOT IN (SELECT ANIMAL_ID
-                             FROM ANIMAL_OUTS)
-    ORDER   BY DATETIME ASC)
-WHERE   ROWNUM <= 3;
+SELECT  NAME, DATETIME
+FROM    ANIMAL_INS
+WHERE   ANIMAL_ID IN    (SELECT  ANIMAL_ID
+                        FROM    ANIMAL_INS
+                        MINUS
+                        SELECT  ANIMAL_ID
+                        FROM    ANIMAL_OUTS)
+ORDER   BY DATETIME
+FETCH   NEXT 3 ROWS ONLY;
