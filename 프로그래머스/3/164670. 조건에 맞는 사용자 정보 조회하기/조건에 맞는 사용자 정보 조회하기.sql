66ed1a8ce63cee6ae432c362650b7a8d01c93c0a
tori <63828057+sangeun99@users.noreply.github.com>
2023-11-13 10:18:23 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,10 +1,9 @@
-SELECT  B.WRITER_ID
-        , U.NICKNAME
-        , U.CITY || ' ' || U.STREET_ADDRESS1 || ' ' || U.STREET_ADDRESS2 AS "전체주소"
-        , SUBSTR(U.TLNO, 1,3) || '-' || SUBSTR(U.TLNO, 4,4) || '-' || SUBSTR(U.TLNO, 8,4) AS "전화번호"
-FROM    (SELECT  WRITER_ID
-        FROM    USED_GOODS_BOARD
-        GROUP   BY WRITER_ID
-        HAVING  COUNT(BOARD_ID) >= 3) B, USED_GOODS_USER U
-WHERE   B.WRITER_ID = U.USER_ID
-ORDER   BY U.USER_ID DESC;
+SELECT  USER_ID, NICKNAME
+        , CITY || ' ' || STREET_ADDRESS1 || ' ' || STREET_ADDRESS2 "전체주소"
+        , SUBSTR(TLNO, 1, 3) || '-' || SUBSTR(TLNO, 4, 4) || '-' || SUBSTR(TLNO, 8, 4) "전화번호"
+FROM    USED_GOODS_USER
+WHERE   USER_ID IN  (SELECT  WRITER_ID
+                    FROM    USED_GOODS_BOARD
+                    GROUP   BY WRITER_ID
+                    HAVING  COUNT(WRITER_ID) >= 3)
+ORDER   BY USER_ID DESC;
