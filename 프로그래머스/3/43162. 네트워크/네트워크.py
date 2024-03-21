40d1200f35876774ac9603875d8f1e704dd98ba0
tori <63828057+sangeun99@users.noreply.github.com>
2024-03-21 12:08:29 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,23 +1,21 @@
-def find(i, network):
-    if (network[i] != i):
-        network[i] = find(network[i], network)
-    return network[i]
-
-def union(i, j, network):    
-    network[max(i, j)] = min(i, j)
+from collections import deque
 
 def solution(n, computers):
-    network = [i for i in range(n)]
+    def bfs(q):
+        while q:
+            i = q.popleft()
+            if visited[i] == 0:
+                visited[i] = 1
+                for c in range(n):
+                    if computers[i][c] == 1 and visited[c] == 0:
+                        q.append(c)
+                
+    answer = 0
+    visited = [0 for _ in range(n)]
+    q = deque([])
     for i in range(n):
-        for j in range(n):
-            if (computers[i][j] == 1) and (i != j):
-                # 둘의 대표 노드 찾기
-                n_i = find(i, network)
-                n_j = find(j, network)
-                # 둘 연결하기
-                union(n_i, n_j, network)
-    for i in range(n):
-        find(i, network)
-    n_set = set(network)
-    answer = len(n_set)
+        if not visited[i]:
+            q.append(i)
+            bfs(q)
+            answer += 1
     return answer
