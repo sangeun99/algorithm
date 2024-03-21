021d0d4ee36d9e6765c84441c8ec4cb5eb8c99e8
tori <63828057+sangeun99@users.noreply.github.com>
2024-03-21 14:03:12 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,44 +1,24 @@
 from collections import deque
 
-def bfs(words, target, changeable, q):
-    visited = [0 for _ in range(len(changeable))]
-    while q:
-        index, depth = q.popleft()
-        if visited[index] == 0:
-            visited[index] = 1
-            if (words[index] == target):
-                return depth
-            for j in range(len(changeable[index])):
-                if changeable[index][j] == 1 and visited[j] == 0:
-                    q.append([j, depth + 1])
-    return 0
+def isChangable(b, t):
+    n = len(b)
+    diff = 0
+    for i in range(n):
+        if b[i] != t[i]:
+            if diff != 0:
+                return False
+            diff += 1
+    return True
 
 def solution(begin, target, words):
-    w_count = len(words)
-    changeable = [[0 for _ in range(w_count)] for _ in range(w_count)]
-    
-    # begin에서 words[i]로 변환 가능한지 확인
-    q = deque()
-    for i in range(w_count):
-        b_match = 0
-        for k in range(len(words[i])):
-            if words[i][k] == begin[k]:
-                continue
-            else:
-                b_match += 1        
-        if b_match == 1:
-            q.append([i, 1]) # depth 함께 넣어주기
-            
-    # words[i]가 words[j]로 변환 가능한지 확인
-    for i in range(w_count):
-        for j in range(w_count):
-            match = 0           
-            for k in range(len(words[i])):
-                if words[i][k] == words[j][k]:
-                    continue
-                else:
-                    match += 1
-            if match == 1:
-                changeable[i][j] = 1
-    answer = bfs(words, target, changeable, q)
-    return answer
+    q = deque([(begin, 0)])
+    if target not in words:
+        return 0
+    while q:
+        w, c = q.popleft()
+        if w == target:
+            return c
+        for i in range(len(words)):
+            if isChangable(w, words[i]):
+                q.append((words[i], c+1))        
+    return 0
